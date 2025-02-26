c01aa0a236e9d17bf4b23cc25de46dd02d8424c2
NathanaelGilberthLaptop <nathanaelgilberth353@gmail.com>
2025-02-26 20:29:25 +0700

    Commit first

---
+++ LibraryManagement/src/Javaconnect.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Christian
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Javaconnect {
+    Connection conn;
+    
+public static Connection ConnecrDb(){
+    try{
+       Class.forName("org.sqlite.JDBC"); 
+       Connection conn = DriverManager.getConnection("jdbc:sqlite:L:\\Tugas Kampus\\Semester 3\\OOP\\LibraryManagement\\LibraryDB.sqlite");
+       return conn;
+    }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+}   
+}
