f33d9868fd25a2072754589283feaebf05dc2193
Nathanael Gilberth Tjandra <nathanaelgilberth353@gmail.com>
2025-04-26 12:48:52 +0700

    Change javaconnect logic
    - Remove getConnection from local file
    - Add function to create new table if not exist
    - Add new sqlite jdbc library

--- LibraryManagement/src/util/Javaconnect.java
+++ LibraryManagement/src/util/Javaconnect.java
@@ -1,28 +1,62 @@
 package util;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Christian
- */
 import java.sql.*;
-import javax.swing.JOptionPane;
 
 public class Javaconnect {
-    Connection conn;
-    
-public static Connection ConnecrDb(){
-    try{
-       Class.forName("org.sqlite.JDBC"); 
-       Connection conn = DriverManager.getConnection("jdbc:sqlite:L:\\Tugas Kampus\\Semester 3\\OOP\\LibraryManagement\\LibraryDB.sqlite");
-       return conn;
-    }catch(Exception e){
-            JOptionPane.showMessageDialog(null, e);
-            return null;
+
+    private static final String DB_URL = "jdbc:sqlite:LibraryDB.sqlite";
+
+    public static Connection connectDb() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection(DB_URL);
+            
+            createTablesIfNotExist(conn);
+            
+            return conn;
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new SQLException("Error connecting to database", e);
         }
-}   
+    }
+
+    private static void createTablesIfNotExist(Connection conn) {
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            String createAccountTableSQL = "CREATE TABLE IF NOT EXISTS Account (" +
+                                          "username TEXT PRIMARY KEY," +
+                                          "password TEXT NOT NULL," +
+                                          "recoveryCode TEXT" +
+                                          ");";
+            stmt.execute(createAccountTableSQL);
+
+            String createBookTableSQL = "CREATE TABLE IF NOT EXISTS Book (" +
+                                        "ID TEXT PRIMARY KEY," +
+                                        "title TEXT NOT NULL," +
+                                        "writer TEXT NOT NULL," +
+                                        "pageCount INTEGER" +
+                                        ");";
+            stmt.execute(createBookTableSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeResources(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
